fdd1773ef8a30e8cf0ec752bb2f126547a6ecc98
aferguson <alexferg727@gmail.com>
2025-10-20 15:10:47 +0200

    change view of /submit path

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-app.get("/submit", (req,res) => {
+app.post("/submit", (req,res) => {
   res
   .type('html')
   .send('hello' + req.query["name"])
